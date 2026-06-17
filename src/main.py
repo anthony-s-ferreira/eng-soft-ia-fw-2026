98be79ca00b8f451699a447a0c01a89ec10c59ab
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2026-06-17 10:26:19 -0300

    feat: add data processing pipeline methods

--- src/main.py
+++ src/main.py
@@ -2,7 +2,12 @@ import data_loader
 import preprocess
 
 def main():
-    pass
+    data = data_loader.load_data("../data")
+    cleaned_data = preprocess.clean_data(data)
+    normalized_data = preprocess.normalize_columns(cleaned_data)
+    train_data, test_data = preprocess.split_data(normalized_data)
+    print(train_data.head())
+    print(test_data.head())
 
 if __name__ == "__main__":
     main()
