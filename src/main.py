d81a07fd25084d125db32eb34f7b724b029e98b8
JhonataLimaSouza <jhonata.amduscia@gmail.com>
2026-06-17 20:31:07 -0300

    Adjusting the data path in the Main File

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import data_loader
 import preprocess
+from pathlib import Path
 
 def main():
-    data = data_loader.load_data("../data")
+    BASE_DIR = Path(__file__).resolve().parent
+    DATA_PATH = BASE_DIR.parent / "data"
+    data = data_loader.load_data(str(DATA_PATH))
     cleaned_data = preprocess.clean_data(data)
     normalized_data = preprocess.normalize_columns(cleaned_data)
     train_data, test_data = preprocess.split_data(normalized_data)
